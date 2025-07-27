7dfd07f31783f5d5eb820bc2c7c54518aea253e5
nikmen <menounosnikitas@gmail.com>
2025-07-27 14:40:02 +0300

    Add RSA support to encryption utilities with display and string conversion

--- src/utils/EncryptionUtils.h
+++ src/utils/EncryptionUtils.h
@@ -2,6 +2,7 @@
 #define ENCRYPTION_UTILS_H
 
 #include "GlobalConfig.h"
+#include <vector>
 
 namespace EncryptionUtils {
 
@@ -11,6 +12,8 @@ inline int toDropdownIndex(EncryptionType type) {
             return 0;
         case EncryptionType::LFSR:
             return 1;
+        case EncryptionType::RSA:
+            return 2;
         default:
             return 0; // Default to AES
     }
@@ -22,9 +25,48 @@ inline EncryptionType fromDropdownIndex(int index) {
             return EncryptionType::AES;
         case 1:
             return EncryptionType::LFSR;
+        case 2:
+            return EncryptionType::RSA;
         default:
             return EncryptionType::AES; // Default to AES
     }
+}
+
+inline std::string encryptionTypeToString(EncryptionType type) {
+    switch (type) {
+        case EncryptionType::AES:
+            return "AES-256";
+        case EncryptionType::LFSR:
+            return "LFSR";
+        case EncryptionType::RSA:
+            return "RSA-2048";
+        default:
+            return "Unknown";
+    }
+}
+
+// Get display name for encryption type (for UI)
+inline const char* getDisplayName(EncryptionType type) {
+    switch (type) {
+        case EncryptionType::AES: return "AES-256";
+        case EncryptionType::LFSR: return "LFSR";
+        case EncryptionType::RSA: return "RSA-2048";
+        default: return "Unknown";
+    }
+}
+
+// Get all available encryption types
+inline std::vector<EncryptionType> getAllTypes() {
+    return {
+        EncryptionType::AES,
+        EncryptionType::LFSR,
+        EncryptionType::RSA
+    };
+}
+
+// Get default encryption type
+inline EncryptionType getDefault() {
+    return EncryptionType::AES;
 }
 
 }
