c3cfbed1e0f1945bda8cc016c078281ae8f67a35
nikmen <menounosnikitas@gmail.com>
2025-08-06 11:39:23 +0300

    Add EncryptionParamsBuilder class for creating encryption parameters

---
+++ src/core/EncryptionParamsBuilder.h
@@ -0,0 +1,47 @@
+#ifndef ENCRYPTION_PARAMS_BUILDER_H
+#define ENCRYPTION_PARAMS_BUILDER_H
+
+#include "../crypto/encryption_factory.h"
+#include "../config/GlobalConfig.h"
+
+/**
+ * @brief Builder class for creating EncryptionConfigParameters consistently
+ * 
+ * This class eliminates code duplication by providing a single place to build
+ * encryption parameters with common defaults from the global configuration.
+ */
+class EncryptionParamsBuilder {
+public:
+    /**
+     * @brief Create encryption parameters with defaults from global config
+     * 
+     * @param type The encryption type
+     * @param masterPassword The master password
+     * @param salt Optional salt (for LFSR)
+     * @return EncryptionConfigParameters Ready-to-use parameters
+     */
+    static EncryptionConfigParameters create(
+        EncryptionType type, 
+        const std::string& masterPassword = "",
+        const std::string& salt = ""
+    );
+
+    /**
+     * @brief Create RSA encryption parameters with key data
+     * 
+     * @param masterPassword The master password
+     * @param publicKey The RSA public key (optional)
+     * @param privateKey The RSA private key (optional)
+     * @return EncryptionConfigParameters Ready-to-use RSA parameters
+     */
+    static EncryptionConfigParameters createRSA(
+        const std::string& masterPassword,
+        const std::string& publicKey = "",
+        const std::string& privateKey = ""
+    );
+
+private:
+    EncryptionParamsBuilder() = default; // Static class
+};
+
+#endif // ENCRYPTION_PARAMS_BUILDER_H
