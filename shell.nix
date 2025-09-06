e4514197d068ce25166262c681dca575b127293b
Nikitas Menounos <82477309+nikitasmen@users.noreply.github.com>
2025-09-06 19:08:48 +0300

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -38,11 +38,13 @@ pkgs.mkShell {
 
   shellHook = ''
     echo "Password Manager development environment loaded!"
-    echo "Run './build.sh' to build the project"
 
     export CMAKE_PREFIX_PATH="${pkgs.fltk}/lib/cmake:$CMAKE_PREFIX_PATH"
     export PKG_CONFIG_PATH="${pkgs.fltk}/lib/pkgconfig:${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.fontconfig}/lib/pkgconfig:${pkgs.expat}/lib/pkgconfig:$PKG_CONFIG_PATH"
-  '';
+    
+    echo "Runnig './build.sh' to build the project"
+    ./build.sh
+'';
 }
 
 
