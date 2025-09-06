3a9ee9f8d11d7005f7fbd45aa180aa96af17d542
nikitasmen <menounosnikitas@gmail.com>
2025-09-06 18:48:50 +0300

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -15,6 +15,14 @@ pkgs.mkShell {
     xorg.libXft
     xorg.libXinerama
 
+    # OpenGL
+    mesa
+    mesa_glu
+
+    # Fontconfig and XML parser
+    fontconfig
+    expat
+
     # System libraries
     openssl
     curl
@@ -23,15 +31,18 @@ pkgs.mkShell {
     gdb
     valgrind
 
-    # For clipboard functionality
+    # Clipboard
     xclip
     wl-clipboard
   ];
 
-  shellHook = '''
+  shellHook = ''
     echo "Password Manager development environment loaded!"
     echo "Run './build.sh' to build the project"
+
     export CMAKE_PREFIX_PATH="${pkgs.fltk}/lib/cmake:$CMAKE_PREFIX_PATH"
-    export PKG_CONFIG_PATH="${pkgs.fltk}/lib/pkgconfig:${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH"
-  ''';
+    export PKG_CONFIG_PATH="${pkgs.fltk}/lib/pkgconfig:${pkgs.openssl.dev}/lib/pkgconfig:${pkgs.fontconfig}/lib/pkgconfig:${pkgs.expat}/lib/pkgconfig:$PKG_CONFIG_PATH"
+  '';
 }
+
+
